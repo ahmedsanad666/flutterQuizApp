a932b487798021affe46cdd244fe725eeb41e67a
ahmedsanad <sanad@tekgroup.app>
2024-06-23 12:18:01 +0300

    fist commit form brcnh 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
+  // first commit form branch 2
   runApp(const MaterialApp(
     home: Scaffold(
       body: StartScreen(),
