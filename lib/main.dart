ff2838b182b45c2902473403f3ead155dd3655c6
ahmedsanad <sanad@tekgroup.app>
2024-06-23 14:27:10 +0300

    from bracnh2 file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  // form branch 1
+  // form branch 1 form pbascdn 2
   runApp(const MaterialApp(
     home: Scaffold(
       body: StartScreen(),
