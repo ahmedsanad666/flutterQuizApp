f64b0003fa7ba5373d0b5c91863cceca56072b1d
ahmedsanad <sanad@tekgroup.app>
2024-06-23 14:13:42 +0300

    add cahnges in branchONe

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  // form branch 1
+  // form branch 1 with new achante 
   runApp(const MaterialApp(
     home: Scaffold(
       body: StartScreen(),
