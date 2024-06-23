6cc7d0e00e5a9f6354ac181e487d9b60aa48a82e
ahmedsanad <sanad@tekgroup.app>
2024-06-23 11:47:12 +0300

    first init form branch one

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
 
 void main() {
+  // form branch 2 
   runApp(const MaterialApp(
     home: Scaffold(
       body: StartScreen(),
